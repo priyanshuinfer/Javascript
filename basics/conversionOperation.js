8a23a2c2f834aa7fb557eba882f23fe82bdccb7b
Priyanshu Chaturvedi <93399628+priyanshuinfer@users.noreply.github.com>
2025-07-16 11:56:17 +0000

    datatypes conversion

---
+++ basics/conversionOperation.js
@@ -0,0 +1,24 @@
+let score = null
+
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber) //returns NaN as input is both numerical and alphabet //return 0 when input is null
+
+// "33" => 33
+// "33abc" => NaN (NOT A NUMBER)
+// true => 1; false => 0
+
+let isLoggedIn = "Priyanshu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "hitesh" => true
+
+let numberone = 33
+
+let stringNumber = String(numberone)
+console.log(typeof stringNumber)
