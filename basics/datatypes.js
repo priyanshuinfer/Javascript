914d82e272e024b1c6d76028351a64acc69327b9
Priyanshu Chaturvedi <93399628+priyanshuinfer@users.noreply.github.com>
2025-07-16 08:33:31 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all JS Code as newer version
+
+//alert("hello") //we are 
+let name = "Priyanshu"
+let age=23
+let isLoggedIn = false
+let state
+
+console.log(typeof "Priyanshu")
---
+++ basics/variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accounrEmail = "priyanshu@gmail.com"
+var accountPasswd = "12345"
+accountCity = "Kolkata"
+let accountState
+//accountId = 2
+
+accounrEmail = "hc@hc.com"
+accountPasswd = "212121"
+accountCity = "Pune"
+
+console.log(accountId);
+
+console.table([accountId,accounrEmail,accountPasswd,accountCity,accountState]);
