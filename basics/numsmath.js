941c123adda8f2c2b907ab1deba222fcbc6191cf
Priyanshu Chaturvedi <93399628+priyanshuinfer@users.noreply.github.com>
2025-07-17 08:52:06 +0000

    numsmath

---
+++ basics/numsmath.js
@@ -0,0 +1,16 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length)
+
+console.log(balance.toFixed(2))
+
+const otherNumber = 1123.8966
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+
+console.log(hundreds.toLocaleString('en-IN'))
