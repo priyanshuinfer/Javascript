8b008ebd1bb4a61f305b4afb5b439a03f824b0c2
Priyanshu Chaturvedi <93399628+priyanshuinfer@users.noreply.github.com>
2025-07-21 18:01:10 +0000

    map&forEach

---
+++ iterations/five.js
@@ -0,0 +1,47 @@
+const coding =["js", "ruby", "java", "python", "cpp"]
+
+
+coding.forEach( function (val){
+    console.log(val)
+} )
+
+
+coding.forEach( (item) => {
+    console.log(item) // Same Output
+}  )
+
+
+function printMe(item){
+    console.log(item) // Same Output
+}
+
+coding.forEach(printMe)  // Same Output
+
+
+coding.forEach( (item, index, arr) => {
+    console.log(item, index, arr)
+
+})
+
+
+
+const myCoding = [
+    {
+        languageName : "javascript",
+        languageFileName : "js"
+    },
+    {
+        languageName : "java",
+        languageFileName : "java"
+    },
+    {
+        languageName : "python",
+        languageFileName : "py"
+    }
+    
+]
+
+
+myCoding.forEach ( (item) => {
+    console.log(item.languageFileName)
+})
---
+++ iterations/four.js
@@ -0,0 +1,20 @@
+const myObject = {
+    js : 'javascript',
+    cpp : 'c++',
+    rb : 'ruby',
+    swift : 'swift by apple'
+}
+
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+}
+
+
+
+const programming = ["js", "rb", "py", "cpp", "java"]
+
+
+for(const key in programming){
+    console.log(programming[key])
+}
