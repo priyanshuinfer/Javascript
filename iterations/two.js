dfeffe7408b3c8af6058042d27acb5c7fa8a92a3
Priyanshu Chaturvedi <93399628+priyanshuinfer@users.noreply.github.com>
2025-07-21 15:38:18 +0000

    dowhile

---
+++ iterations/two.js
@@ -0,0 +1,39 @@
+// while loop
+
+let index = 0
+
+while (index <= 10) {
+    console.log(`Value of index is : ${index}`)
+    index = index + 2
+    
+}
+
+let myArray = ['flash', "batman","superman"]
+
+let arr = 0
+while(arr < myArray.length){
+    console.log(`Value is ${myArray[arr]}`)
+    arr = arr + 1
+}
+
+
+// do while loop
+
+let score = 11 // When score is equal to 10 -> Output 
+// Score is 3
+// Score is 4
+// Score is 5
+// Score is 6
+// Score is 7
+// Score is 8
+// Score is 9
+
+
+// when score is equal to 11 Output -> Score is 11
+
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while(score <= 10){
+
+}
